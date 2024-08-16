ff5e7caed8bc12b632f21e455973c9163257fc25
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 16:00:17 -0300

    Create UsarFormUsuario.java

---
+++ Formularios/ValidacaoUsuario/UsarFormUsuario.java
@@ -0,0 +1,11 @@
+package usarformusuario;
+
+public class UsarFormUsuario {
+
+    public static void main(String[] args) 
+    {
+        FormUsuario formUm = new FormUsuario();
+        
+        formUm.setVisible(true);
+    }
+}

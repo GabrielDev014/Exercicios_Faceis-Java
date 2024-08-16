da69d0713287b90b06c57e8b0b26187b3c9c54d0
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:40:12 -0300

    Update Hospital.java

--- POO/Hospital/Hospital.java
+++ POO/Hospital/Hospital.java
@@ -38,25 +38,3 @@ public class Hospital
       }
     }
 }
-
-package hospital;
-
-public class Paciente 
-{
-    public String nome;
-    public String tipoEnfermidade;
-    
-    public Paciente(String nome, String tipoEnfermidade)
-    {
-        this.nome = nome;
-        this.tipoEnfermidade = tipoEnfermidade;
-    }
-    public void alterarEnfermidade(String tipoEnfermidade)
-    {
-        this.tipoEnfermidade = tipoEnfermidade;
-    }
-    public String relatorioPaciente()
-    {
-        return "individuo " + nome + " doenca " + tipoEnfermidade + "\n";
-    }
-}

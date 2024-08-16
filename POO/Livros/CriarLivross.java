229b134176fcf66f4274d5fea9276d4d6b39a667
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:41:19 -0300

    Update CriarLivross.java

--- POO/Livros/CriarLivross.java
+++ POO/Livros/CriarLivross.java
@@ -24,18 +24,3 @@ public static void main(String[] args)
         System.out.println(livroUm.exibirLivro());
     } 
 }
-
-package criarlivross;
-public class Livro 
-{
-    public String titulo;
-    public String autor;
-    public String editora;
-    public int numeroPaginas;
-    
-    
-    public String exibirLivro()
-    {
-        return "Dados Livro " + titulo + " Escritor " + autor + " editora " + editora + " " + numeroPaginas + " Paginas \n";
-    }
-}

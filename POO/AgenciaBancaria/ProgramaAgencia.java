b6dbb54a3542d2d60cc8eda43e557d9986725523
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:35:36 -0300

    Update ProgramaAgencia.java

--- POO/AgenciaBancaria/ProgramaAgencia.java
+++ POO/AgenciaBancaria/ProgramaAgencia.java
@@ -20,28 +20,3 @@ public class ProgramaAgencia
         System.out.println(contaUm.extratoSimples());
     }
 }
-
-package programaagencia;
-public class ContaBancaria 
-{
-    public String titular;
-    public double saldo;
-    
-    public void saque (double valor)
-    {
-        if (saldo >= valor)
-        {
-            System.out.println("Retirada: " + valor);
-            saldo = saldo - valor;
-        }
-        else
-        {
-            System.out.println("Saldo insuficiente para a retirada de: " + valor);
-        }
-    }
-    
-    public String extratoSimples()
-    {
-        return "Correntista: " + titular + " Valor atual da conta: " + saldo;
-    }      
-}

cdef524f51a96e5bb42abd9fa7c5db6fbe00bae7
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:42:20 -0300

    Create Individuo.java

---
+++ POO/Pessoas/Individuo.java
@@ -0,0 +1,37 @@
+package listaenderecos;
+
+public class Individuo 
+{
+   private Endereco endereco;
+   private String nome;
+   private String cpf;
+   
+   public String getNome()
+   {
+       return nome;
+   }
+   public void setNome(String nome)
+   {
+       this.nome = nome;
+   }
+   public String getCpf()
+   {
+       return cpf;
+   }
+   public void setCpf(String cpf)
+   {
+       this.cpf = cpf;
+   }
+   public Endereco getEndereco()
+   {
+       return endereco;
+   }
+   public void setEndereco(Endereco endereco)
+   {
+       this.endereco = endereco;
+   }
+   public String toString()
+   {
+       return " Nome: " + nome + " Doc: " + cpf + " Logradouro: " + endereco;
+   }
+}

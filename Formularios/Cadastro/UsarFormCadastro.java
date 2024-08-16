a96381359c0dd0b08d0799f2e41da7cc31ab291d
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 16:19:15 -0300

    Main

---
+++ Formularios/Cadastro/UsarFormCadastro.java
@@ -0,0 +1,12 @@
+package usarformcadastro;
+
+public class UsarFormCadastro 
+{
+    public static void main(String[] args) 
+    {
+        //Criando objeto (formUm) que seguirá os padrões da classe FormCadastro
+        FormCadastro formUm = new FormCadastro();
+        //Tornando o formulário visível ao usuário
+        formUm.setVisible(true);
+    } 
+}

9d6a7c44fba1907059d9fcc182e26c901830659c
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:53:50 -0300

    Create UsarFormulario.java

---
+++ Formularios/Primeiro_Form/UsarFormulario.java
@@ -0,0 +1,11 @@
+package usarformulario;
+
+public class UsarFormulario 
+{
+
+    public static void main(String[] args) 
+    {
+        FormularioAluno formUm = new FormularioAluno();
+        formUm.setVisible(true);
+    }
+}

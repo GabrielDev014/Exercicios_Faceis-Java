d5fbac4bddb77bc5e7068275fa95596c2f87f2e2
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:37:22 -0300

    Update EspecificaProfessor.java

--- POO/Escola/EspecificaProfessor.java
+++ POO/Escola/EspecificaProfessor.java
@@ -19,39 +19,3 @@ public class EspecificaProfessor
     }
     
 }
-package especificaprofessor;
-
-public class Professor 
-{
-    private String nome;
-    private int idade;
-    
-    public Professor (String nome, int idade)
-    {
-        this.nome = nome;
-        this.idade = idade;
-    }
-    public void setNomeProfessor(String nome)
-    {
-        this.nome = nome;
-    }
-    public void setIdadeProfessor (int idade)
-    {
-        if (idade > 0 && idade < 130)
-                {
-                    this.idade = idade;
-                }
-        else
-        {
-            this.idade = 0;
-        }
-    }
-    public String getNomeProfessor()
-    {
-        return nome;
-    }
-    public int getIdadeProfessor()
-    {
-        return idade;
-    }
-}

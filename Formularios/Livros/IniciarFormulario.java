782d6f19598141778db2285cd8bb6b063d22ad4d
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:55:50 -0300

    Create IniciarFormulario.java

---
+++ Formularios/Livros/IniciarFormulario.java
@@ -0,0 +1,10 @@
+package iniciarformulario;
+
+public class IniciarFormulario 
+{
+    public static void main(String[] args) 
+    {
+        FormularioLivro formUm = new FormularioLivro();
+        formUm.setVisible(true);
+    }
+}

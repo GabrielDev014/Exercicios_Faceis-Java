be7961e8a2f54c1f63ff41688abd5a71736bcbff
Gabriel Lima <gabriellimaalves.si25@gmail.com>
2024-08-16 15:44:24 -0300

    Update Loja.java

--- POO/Produtos/Loja.java
+++ POO/Produtos/Loja.java
@@ -21,17 +21,3 @@ public class Loja
         System.out.println(produtoUm.exibirProduto());
     }
 }
-
-package loja;
-
-public class Produto 
-{
-    public String nome;
-    public String fabricante;
-    public double valor;
-    
-    public String exibirProduto()
-    {
-        return "Produto Escolhido: " + nome + " Fabricante: " + fabricante + " Valor: " + valor + " Reais \n";
-    }
-}
